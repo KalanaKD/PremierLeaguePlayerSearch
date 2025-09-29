e1ba72432f4f7dfbe61505fcf827fe758d408b86
Kalana_KD <kalanakd471@gmail.com>
2025-09-29 11:00:00 +0530

    Update repo layer with adding deleteById method

--- src/main/java/com/kd/Backend/Repo/PlayerRepo.java
+++ src/main/java/com/kd/Backend/Repo/PlayerRepo.java
@@ -1,8 +1,10 @@
 package com.kd.Backend.Repo;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.kd.Backend.model.Player;
@@ -12,5 +14,12 @@ public interface PlayerRepo extends JpaRepository<Player, String> {
     void deleteByName(String playerName);
 
     Optional<Player> findByName(String playerName);
+
+    Optional<Player> findById(long id);
+
+    @Query("SELECT p FROM Player p WHERE p.club = :club")
+    List<String> findDistinctPositions();
+
+    void deleteById(long id);
 
 }
